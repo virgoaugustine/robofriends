87cf975a7aced03da9fad4e12fca803dc3e7a142
virgoaugustine <virgoaugustine@gmail.com>
2020-05-06 11:04:24 +0000

    Updates

--- precache-manifest.1fc199cc0d71fc189afd7f74d52b5264.js
+++ precache-manifest.1fc199cc0d71fc189afd7f74d52b5264.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d70184a44824e2c6573be418dd4e13bc",
+    "revision": "706489c9030da95cd480aa952270595a",
     "url": "/robofriends/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/css/2.036b41f9.chunk.css"
   },
   {
-    "revision": "62cd6f18fa7ef9ba8f05",
-    "url": "/robofriends/static/css/main.51dc3192.chunk.css"
+    "revision": "82d176e60672c0b27b9d",
+    "url": "/robofriends/static/css/main.5854e501.chunk.css"
   },
   {
     "revision": "4cd627e2823614a3a0e8",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/js/2.7dfcfc6c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "62cd6f18fa7ef9ba8f05",
-    "url": "/robofriends/static/js/main.f37e8f5e.chunk.js"
+    "revision": "82d176e60672c0b27b9d",
+    "url": "/robofriends/static/js/main.1827ce19.chunk.js"
   },
   {
     "revision": "4b54cd1813776aa306f7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.4863b1db1b5015b48c24d302fa1846af.js"
+  "/robofriends/precache-manifest.1fc199cc0d71fc189afd7f74d52b5264.js"
 );
 
 self.addEventListener('message', (event) => {
