758e3e6eb7a32b14ff75f3b6147ac3250ea20c0b
virgoaugustine <virgoaugustine@gmail.com>
2020-05-06 02:05:52 +0000

    Updates

---
+++ precache-manifest.f71be6426b640c070202e1f7740c9492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f3586e4e40d0c863598be34a513ca0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4cd627e2823614a3a0e8",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c870990e529902bfa236",
+    "url": "/robofriends/static/css/main.c4290b1d.chunk.css"
+  },
+  {
+    "revision": "4cd627e2823614a3a0e8",
+    "url": "/robofriends/static/js/2.7dfcfc6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7dfcfc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c870990e529902bfa236",
+    "url": "/robofriends/static/js/main.9e5cce3b.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
